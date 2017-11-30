f840622f2f70a4a46741bfc1f90c39a9fb450dba
Tran T. Ngoc <ttngoc653@gmail.com>
2017-11-30 08:02:37 +0700

    Add files via upload

---
+++ bootstrap_component/src/component/content.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+
+
+class Content extends Component{
+	render(){
+		return(
+			<div className="abc">Hisimaro Zoro</div>
+		);
+	}
+}
+
+export default Content;

42bb145d5656112f1787ed83a8243eef3c15fa68
Tran T. Ngoc <ttngoc653@gmail.com>
2023-10-22 18:00:14 +0700

    add featured item (to 44:00)

--- jossadmin/src/admin/pages/dashboard/Dashboard.jsx
+++ jossadmin/src/admin/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./dashboard.scss"
 
 export const Dashbroad = () => {
   return (
     <div className="dashbroad">
         <h1>Dashboard</h1>
+        <FeaturedInfo/>
     </div>
   )
 }
